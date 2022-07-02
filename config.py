1346c5cb147e6ab290c3ac47a3f4e302357cc452
Chiktanshu <95119314+Chiktanshu@users.noreply.github.com>
2022-07-02 12:47:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ if os.path.exists("local.env"):
 load_dotenv()
 admins = {}
 
-SESSION_NAME = getenv("SESSION_NAME")
+SESSION_NAME = getenv("BQB7VGOulOsQxcDhtv0Dk8ftv62F8tEmK9yTtaNbiVbSDCbKQsEnwP3rXVMV3myFTc065qjn3Qu84IvWdFzZQGAUnSxAJnrcGKQNbjZAh89ty5XEMXrwQ2b_6488nU9RiQb8Co7TKhPQifG3AjDbx9_ZLFqi3ln5Dcna3MshsZTbUur-DmvdgIR9sPW68g7zzkhl-Cdr-GFboQ5KVnmCMFkAnV28claOELVkD0r7blbSF55E81vQ7ZKsHbZx9iYWDsdGxiSecYB63zkMDuR_8kaGOcm1isIDxILPuc9dZGSUx12zIqHYb4A9jkxZBqOSBp1g7NlYsVz101BhMkkWOZO_AAAAAGtsabsA")
 
 if str(getenv("STRING_SESSION2")).strip() == "":
     SESSION2 = str(None)
@@ -33,23 +33,23 @@ if str(getenv("STRING_SESSION5")).strip() == "":
 else:
     SESSION5 = str(getenv("STRING_SESSION5"))
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-BOT_NAME = getenv("BOT_NAME", "Umk")
+BOT_TOKEN = getenv("BOT_TOKEN", "2141828349:AAGPx5FxotDoFHTcYYcoVR9sPFFreTo_248")
+BOT_NAME = getenv("BOT_NAME", "Music Botto")
 
-API_ID = int(getenv("API_ID", "8186557"))
-API_HASH = getenv("API_HASH", "efd77b34c69c164ce158037ff5a0d117")
+API_ID = int(getenv("API_ID", "4329428"))
+API_HASH = getenv("API_HASH", "da3cda55bca8a7d7cc14e5d294a62d0e")
 MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://Cloner:Cloner@cluster0.cgc6t.mongodb.net/?retryWrites=true&w=majority")
-OWNER_NAME = getenv("OWNER_NAME", "Zaid")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "Timesisnotwaiting")
-ALIVE_NAME = getenv("ALIVE_NAME", "Zaid")
-BOT_USERNAME = getenv("BOT_USERNAME", "Zaid2_Robot")
-OWNER_ID = getenv("OWNER_ID", "1669178360")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Zaid2_Assistant")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "TheSupportChat")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "TheUpdatesChannel")
+OWNER_NAME = getenv("OWNER_NAME", "Nikarshu")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "Canyousenseme_bitch")
+ALIVE_NAME = getenv("ALIVE_NAME", "Nikarshu")
+BOT_USERNAME = getenv("BOT_USERNAME", "WCC_musicbot")
+OWNER_ID = getenv("OWNER_ID", "1802267067")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Hejiullah_wooohuuuu")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "weebschitchat")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Weeb_Caffe_Updates")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("UPDATES_CHANNEL", "HEROKU_API_KEY")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1669178360").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1802267067").split()))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/fc9d87ffd1c6f828eb7fc.png")
 START_PIC = getenv("START_PIC", "https://telegra.ph/file/a414e2cdfeaa7d4414b89.png")
